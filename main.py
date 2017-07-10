6352e36b7837d0e9797c6c72f874c94264de4bab
Himura Kazuto <Himura2la@users.noreply.github.com>
2017-07-10 11:10:28 +0300

    logging update

--- main.py
+++ main.py
@@ -22,31 +22,38 @@ t = Tester(s)
 o2.RGB_color(0, 0, 0)
 
 def wifi_reconnect():
+    i.notice("Restarting Wi-Fi...")
     subprocess.call("wifi", shell=True)
     i.notice("Waiting 5s for link to raise...")
     time.sleep(5)
 
 def router_notice():
-    i.notice("(ಠ_ಠ) Link failed once. Waiting 30s...")
+    i.notice("(¬_¬) Link failed once. Waiting 30s...")
     o2.RGB_color(0, 0, 100)
     time.sleep(30)
     wifi_reconnect()
 
 def router_warning():
-    i.notice("(¬_¬) Link failed twice. Waiting 2m...")
+    i.notice("(ಠ_ಠ) Link failed twice. Waiting 2m...")
     o2.RGB_color(100, 1, 0) # Yellow
     time.sleep(60 * 2)
     wifi_reconnect()
 
 def router_is_dead():
     o2.RGB_color(100, 0, 0)
-    i.warning("(╯°□°）╯ Link is Dead. Rebooting router.")
+    i.crytical("(ﾉ°□°)ﾉ Link is Dead. Rebooting router.")
     if not r.soft_reboot():
         i.warning("Soft reboot failed!")
         r.hard_reboot()
     i.warning("Waiting 1m for router to reboot...")
     time.sleep(60)
     wifi_reconnect()
+    
+    i.notice("Recovering sequence finished.")
+    if t.ping("ya.ru", 3):
+        i.notice("\(‘ ∇‘ )/ Link was raised!")
+    else:
+        i.notice("(╥﹏╥) Nothing helps...")
 
 def check_router():
     if not t.ping("ya.ru", 3):
@@ -57,9 +64,9 @@ def check_router():
                 router_is_dead()
                 return False
             else:
-                i.notice("ヽ(‘ ∇‘ )ノ Link raised by itself.")
+                i.notice("\(‘ ∇‘ )/ Link raised by itself.")
         else:
-            i.notice("ヽ(‘ ∇‘ )ノ Link raised by itself.")
+            i.notice("\(‘ ∇‘ )/ Link raised by itself.")
     i.ok("Router")
     o2.RGB_color(0, 0, 0)
     return True
@@ -67,20 +74,21 @@ def check_router():
 # ------------------------------------------
 
 def server_warning():
-    i.notice("(ಠ_ಠ) Server's connection fails. Waiting 1m.")
+    i.notice("(¬_¬) Server's connection fails. Waiting 1m.")
     time.sleep(60)
     i.notice("┴─┴ ノ( º _ ºノ) Maybe router is dead? Checking this.")
     router_status = check_router()
     i.notice("Router checking result: " + str(router_status))
 
 def server_is_dead():
-    i.warning("(╯°□°）╯ Server is Dead. Rebooting.")
+    i.crytical("(ﾉ°□°)ﾉ Server is Dead. Rebooting.")
     if s.soft_reboot():
         i.warning("Waiting 3m for server to reboot...")
         time.sleep(60 * 3)
     else:
         i.crytical("¯\_(ツ)_/¯ Failed ro reboot. Giving up.")
-
+    i.notice("Recovering sequence finished.")
+    
 def check_server():
     if not t.remote_ping("ya.ru", 3):
         server_warning()
@@ -88,7 +96,7 @@ def check_server():
             server_is_dead()
             return False
         else:
-            i.notice("ヽ(‘ ∇‘ )ノ Server raised by itself.")
+            i.notice("\(‘ ∇‘ )/ Server raised by itself.")
     else:
         i.ok("Server")
         return True
